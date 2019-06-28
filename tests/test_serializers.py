b92df2a48e68ab58fe5553d1f004931ef7a97d74
Serkan Hosca <serkan@hosca.com>
2019-06-28 19:00:38 -0400

    Handle dict attrs

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 import dataclasses as da
 import enum
-from typing import List
+from typing import Dict, List
 
 from django.core.exceptions import ValidationError as DjangoValidationError
 from django.test import SimpleTestCase
@@ -42,6 +42,24 @@ class Line:
 class Geometry:
     lines: List[Line] = da.field(default=None)
     color: Color = da.field(default=None)
+
+
+@da.dataclass
+class Address:
+    street: str = da.field(default=None)
+    city: str = da.field(default=None)
+
+
+@da.dataclass
+class Person:
+    name: str = da.field(default=None)
+
+    addresses: Dict[str, Address] = da.field(default=None)
+
+
+@da.dataclass
+class Dummy:
+    stuff: Dict[str, int] = da.field(default=None)
 
 
 class TestModelSerializer(SimpleTestCase):
@@ -344,7 +362,7 @@ class TestModelSerializer(SimpleTestCase):
         serializer.is_valid(raise_exception=True)
         geometry = serializer.save()
 
-        self.assertDictEqual(da.asdict(geometry), {"color": None, "lines": None})
+        self.assertDictEqual(da.asdict(geometry), {"color": None, "lines": []})
 
     def test_nested_list_disable_nested_update(self):
         class Serializer(DataclassSerializer):
@@ -376,3 +394,65 @@ class TestModelSerializer(SimpleTestCase):
                 "lines": [{"a": {"x": 1, "y": 2}, "b": {"x": 3, "y": 4}}, {"a": {"x": 5, "y": 6}, "b": None}],
             },
         )
+
+    def test_nested_dict_with_dataclass_serializer(self):
+        class Serializer(DataclassSerializer):
+            class Meta:
+                model = Person
+                fields = "__all__"
+
+        serializer = Serializer(
+            data={"name": "Sherlock Holmes", "addresses": {"work": {"street": "221B Baker Street", "city": "London"}}}
+        )
+        serializer.is_valid(raise_exception=True)
+        person = serializer.save()
+
+        self.assertIsInstance(person.addresses["work"], Address)
+        self.assertDictEqual(
+            da.asdict(person),
+            {"name": "Sherlock Holmes", "addresses": {"work": {"street": "221B Baker Street", "city": "London"}}},
+        )
+
+    def test_nested_dict_with_dataclass_serializer_disable_nested_update(self):
+        class Serializer(DataclassSerializer):
+            class Meta:
+                model = Person
+                fields = "__all__"
+                extra_kwargs = {"addresses": {"allow_nested_updates": False, "allow_create": False}}
+
+        instance = Person(addresses={"work": Address(street="221B Baker Street", city="London")})
+
+        serializer = Serializer(
+            instance,
+            data={
+                "name": "Sherlock Holmes",
+                "addresses": {"work": {"street": "Empire State Building", "city": "New York"}},
+            },
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        person = serializer.save()
+
+        self.assertIsInstance(person.addresses["work"], Address)
+        self.assertDictEqual(
+            da.asdict(person),
+            {"name": "Sherlock Holmes", "addresses": {"work": {"street": "221B Baker Street", "city": "London"}}},
+        )
+
+    def test_nested_dict_with_field_serializer(self):
+        class Serializer(DataclassSerializer):
+            class Meta:
+                model = Dummy
+                fields = "__all__"
+
+        serializer = Serializer(data={})
+        serializer.is_valid(raise_exception=True)
+        dummy = serializer.save()
+
+        self.assertDictEqual(da.asdict(dummy), {"stuff": None})
+
+        serializer = Serializer(data={"stuff": {"a": 1, "b": 2}})
+        serializer.is_valid(raise_exception=True)
+        dummy = serializer.save()
+
+        self.assertDictEqual(da.asdict(dummy), {"stuff": {"a": 1, "b": 2}})
